24c97837952005b7ac1cafb4efd1fd9bde93d7ad
Renan Cunha <renan.cunha20@gmail.com>
2016-09-12 12:04:40 -0300

    delete

--- control/deleteAtivo.php
+++ control/deleteAtivo.php
@@ -4,7 +4,7 @@ $ID = $_POST['nome'];
     //verifica se o ID esta vazio
 if (isset($_POST['nome'])) {
     //delete e script para voltar ao index
-	mysqli_query($conexao,"DELETE * FROM ATIVO WHERE cd_patrimonio = '$ID' ");
+	mysqli_query($conexao,"DELETE FROM ATIVO WHERE cd_patrimonio = '$ID' ");
     print "sucesso";
 } else {
     //erro e script para voltar ao index
